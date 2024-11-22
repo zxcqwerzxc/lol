352ebb9e6947a0439a93c474a46e244eb660ef5d
zxcqwerzxc <annihomiki.com@gmail.com>
2024-11-22 09:23:18 +0300

    класс артикл сделан

--- TeamProjеct/Article.cs
+++ TeamProjеct/Article.cs
@@ -8,5 +8,30 @@ namespace TeamProjеct
 {
     internal class Article
     {
+        //класс персон public Person Author { get; set; }
+        public string Title { get; set; }
+        public double Rating { get; set; }
+
+        // Конструктор с параметрами
+        public Article(string title, double rating)//Person author,
+        {
+            //       Author = author;
+            Title = title;
+            Rating = rating;
+        }
+
+        // Конструктор без параметров с значениями по умолчанию
+        public Article()
+        {
+            //      Author = new Person("Unknown Author", 0);
+            Title = "Untitled Article";
+            Rating = 0.0;
+        }
+
+        // Перегруженная версия метода ToString()
+        public override string ToString()
+        {
+            return $"Article Title: {Title}  Rating: {Rating}";//Author: {Author}
+        }
     }
 }
