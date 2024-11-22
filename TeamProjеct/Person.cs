36876e7a4343f37eee3b17c4bfbe5d938e8e9e81
Hannah <arhipceva8@gmail.com>
2024-11-22 16:00:59 +0300

    легендарно все пофиксила

--- TeamProjеct/Person.cs
+++ TeamProjеct/Person.cs
@@ -8,5 +8,74 @@ namespace TeamProjеct
 {
     internal class Person
     {
+        String firstName;
+        String lastName;
+        DateTime birthday;
+
+        public DateTime Birthday
+        {
+            get => birthday;
+            set
+            {
+                if (DateTime.Today >= value)
+                    birthday = value;
+                else
+                    throw new ArgumentException("День рождения не может быть больше сегодняшнего дня");
+            }
+        }
+        public String FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException("Значение имеет null или пустая строка");
+
+                firstName = value;
+            }
+        }
+        public String LastName
+        {
+            get => lastName;
+            set
+            {
+                if (String.IsNullOrEmpty(value))
+                    throw new ArgumentException("Значение имеет null или пустая строка");
+
+                lastName = value;
+            }
+        }
+
+        public int Yers
+        {
+            get => birthday.Year;
+            set
+            {
+                if (value > DateTime.Now.Year)
+                    throw new ArgumentException("Передаваемый год больше года в данный момент");
+
+                int differenceYers = DateTime.Now.Year - value;
+
+                birthday = birthday.AddYears(-differenceYers);
+            }
+        }
+
+        public Person()
+        {
+            this.firstName = "Имя неизвестно";
+            this.lastName = "Фамилия неизвестна";
+            this.birthday = DateTime.Today;
+        }
+
+        public Person(String firstName, String lastName, DateTime birthday)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
+        }
+
+        public override string ToString() => $"Имя: {firstName}; Фамилия: {LastName}; год рождения: {birthday.ToShortDateString()}";
+
+        public virtual string ToShortString() => $"Имя: {firstName}; Фамилия: {LastName}";
     }
 }
